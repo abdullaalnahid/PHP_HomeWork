f5153111bfe85740eaf568c5ed9c4597c479e165
Abdulla Al Nahid <97011163+abdullaalnahid@users.noreply.github.com>
2022-02-09 23:13:09 +0600

    Form-Handling

---
+++ Form-Handling/Backup.php
@@ -0,0 +1,7 @@
+<html>
+    <body>
+       <b>Welcome</b>  <?php echo $_POST["usertname"]; ?> <br>
+        <b>Your Address is:</b> <?php echo $_POST["address"]; ?> <br>
+        <b>Your E-mail is:</b> <?php echo $_POST["email"]; ?>
+    </body>
+</html>
---
+++ Form-Handling/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Handling</title>
+</head>
+<body>
+    <form action="Backup.php" method="post">
+        Name: <input type="text" placeholder="Your Name" name="usertname"> <br> <br>
+        Address: <input type="text" placeholder="Your Adderss" name="address"> <br> <br>
+        E-mail: <input type="email" placeholder="Your Email" name="email"> <br> <br>
+        <button>Submit</button>
+    </form>
+</body>
+</html>
